c0a3511c08589091c74a2de4ddea05d8f5d5a921
LAB12_USER ON WS18 <L12X16W15@LabHCI.net>
2026-08-19 17:34:14 +0800

    Print hello world

---
+++ lab_act_2/android/app/src/main/kotlin/com/example/lab_act_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab_act_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab_act_2/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runWidget(MaterialApp(home: Text("Hello World")));
+}
